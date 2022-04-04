c13a538b6f4570e5be039b36be2152c1a6381f27
Alexandra <belliraxia@gmail.com>
2022-04-04 14:06:03 -0400

    adds chance.js

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 /* global Vue, axios, moment */
 
 var moment = require("moment");
+var Chance = require("chance");
 
 console.log(moment().format("MMM Do YY"));
+
+var chance = new Chance();
+var my_random_string = chance.string();
+console.log(my_random_string);
 
 var App = {
   data: function () {
